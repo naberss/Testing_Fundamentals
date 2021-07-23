57585e0436f764b64105a804ae00e1d4c1ad0ad0
Lucas Moreira <lucas-berto@hotmail.com>
2021-07-23 07:12:25 -0300

    [#2 Criado teste de equality] - Incremento
    *Teste de equality utilizando assertEquals e assertNotEquals.

--- src/main/java/com/naberss/testing_fundamentals/Entities/Dollar.java
+++ src/main/java/com/naberss/testing_fundamentals/Entities/Dollar.java
@@ -1,5 +1,7 @@
 package com.naberss.testing_fundamentals.Entities;
 
+import java.util.Objects;
+
 public class Dollar {
 
     private int amount;
@@ -8,8 +10,8 @@ public class Dollar {
         this.amount = amount;
     }
 
-    public void times(int multiplier) {
-        this.amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(this.amount * multiplier);
     }
 
     public int getAmount() {
@@ -18,5 +20,18 @@ public class Dollar {
 
     public void setAmount(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return getAmount() == dollar.getAmount();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAmount());
     }
 }
--- src/test/java/com/naberss/testing_fundamentals/Entities_Test/MoneyTest.java
+++ src/test/java/com/naberss/testing_fundamentals/Entities_Test/MoneyTest.java
@@ -2,15 +2,29 @@ package com.naberss.testing_fundamentals.Entities_Test;
 
 import com.naberss.testing_fundamentals.Entities.Dollar;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
     @Test
-    void testMultiplication(){
+    void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10,five.getAmount());
+        Dollar product = five.times(2);
+        assertEquals(10, product.getAmount());
+        product = five.times(3);
+        assertEquals(15, product.getAmount());
+    }
+
+    @Test
+    void testEquality() {
+        try{
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(5));}
+        catch(AssertionFailedError e){
+            e.printStackTrace();
+        }
     }
 }
