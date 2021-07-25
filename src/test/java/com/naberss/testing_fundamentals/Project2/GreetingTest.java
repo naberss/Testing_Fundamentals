6c35554a80ffb3d08e957a746d3bdc847230a76d
Lucas Moreira <lucas-berto@hotmail.com>
2021-07-25 15:35:02 -0300

    [#7  Estudo JUnit5  LifeCycle ] - Incremento

--- src/test/java/com/naberss/testing_fundamentals/Project2/GreetingTest.java
+++ src/test/java/com/naberss/testing_fundamentals/Project2/GreetingTest.java
@@ -1,5 +1,7 @@
 package com.naberss.testing_fundamentals.Project2;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +11,14 @@ class GreetingTest {
 
     Greeting greeting;
 
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("@BeforeAll this has to be static");
+    }
+
     @BeforeEach
     void setUp() {
-         greeting = new Greeting();
+        greeting = new Greeting();
     }
 
     @Test
@@ -23,7 +30,11 @@ class GreetingTest {
     void helloWorldTest2() {
         Greeting greeting = new Greeting();
         assertEquals("hello lucas", greeting.helloWorld("lucas"));
+    }
 
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("@AfterAll this has to be static");
     }
 
 }
