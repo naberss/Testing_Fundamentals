cc22ceb7925fd85fd9780b1ef10d16a73020a4ef
Lucas Moreira <lucas-berto@hotmail.com>
2021-07-26 06:07:05 -0300

    [#8  Estudo de ferramentas de teste disponibilizadas via Junit] - Incremento
    *@Disabled
    *@DisplayName
    *@AssertAll
    *AssertThrows
    *AssertTimeOut / Preeptively
    *Assumptions

--- src/test/java/com/naberss/testing_fundamentals/Project2/GreetingTest.java
+++ src/test/java/com/naberss/testing_fundamentals/Project2/GreetingTest.java
@@ -1,12 +1,13 @@
 package com.naberss.testing_fundamentals.Project2;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.naberss.testing_fundamentals.Money_Converter.Exceptions.UnregisteredCurrency;
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@Disabled(value = "it happens")
 class GreetingTest {
 
     Greeting greeting;
@@ -21,15 +22,53 @@ class GreetingTest {
         greeting = new Greeting();
     }
 
+    @DisplayName("just testing")
     @Test
     void helloWorldTest() {
-        assertEquals("hello world", greeting.helloWorld());
+        assertAll("Test List 1",
+                () -> assertAll("Test List2",
+                        () -> assertEquals("hello world", greeting.helloWorld()),
+                        () -> assertEquals("hello lucas", greeting.helloWorld("lucas"))),
+                () -> assertEquals("hello world", greeting.helloWorld()),
+                () -> assertEquals("hello lucas", greeting.helloWorld("lucas")));
+
     }
 
+    @Disabled(value = "because i want to")
     @Test
     void helloWorldTest2() {
         Greeting greeting = new Greeting();
         assertEquals("hello lucas", greeting.helloWorld("lucas"));
+        System.out.printf(greeting.helloWorld("lucas"));
+        assertTrue("hello lucas" == greeting.helloWorld("lucas"), () -> "not the same thing");
+    }
+
+
+    @Test
+    void expectedException() {
+        assertThrows(UnregisteredCurrency.class, () -> {
+            throw new UnregisteredCurrency("test");
+        });
+    }
+
+    @Test
+    void timeoutTest() {
+        assertTimeout(Duration.ofMillis(100),
+                () -> Thread.sleep(85));
+    }
+
+    @Disabled("because i want to '2'")
+    @Test
+    void timeoutTestPreemptively() {
+        assertTimeoutPreemptively(Duration.ofMillis(100),
+                () -> Thread.sleep(105));
+    }
+
+    @Test
+    void AssumptionTest() {
+        Assumptions.assumeTrue(1==0);
+        Assumptions.assumeFalse(1==0);
+
     }
 
     @AfterAll
